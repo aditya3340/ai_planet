69b34d2c0be59b44ba742d908f632f0fdae790ac
aditya3340 <kumar.aditya3340@gmail.com>
2023-08-13 19:41:48 +0530

    home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,24 +3,23 @@ import { useSelector } from "react-redux";
 import Card from "../Components/Card";
 import Hero from "../Components/Hero";
 import Navbar from "../Components/Navbar";
-import { Button, SimpleGrid,Text,Box, Heading } from "@chakra-ui/react";
-
+import { Button, SimpleGrid, Text, Box, Heading } from "@chakra-ui/react";
 
 const Home = () => {
   const submissions = useSelector((state) => state.submissions.data);
 
   //to toggle between all and fav submissions
   const [flag, setFlag] = useState(false);
-  
-  console.log(submissions)
 
-  useEffect(()=> {  
-    setFlag(false)
-  }, [])
+  console.log(submissions);
+
+  useEffect(() => {
+    setFlag(false);
+  }, []);
 
   const subEl = submissions.map((item) => {
     return (
-      <div key={item.id} >
+      <div key={item.id}>
         <Card sub={item} />
       </div>
     );
@@ -32,16 +31,35 @@ const Home = () => {
     <div>
       <Navbar />
       <Hero />
-      <Box maxW ={"85%"} margin={"auto"} marginTop={"10px"}>
-      <SimpleGrid columns={[1,2,2]} maxW={{sm: "auto", md:"50%", lg:"40%"}}>
-        <Button m={"10px"} paddingInline={"20px"} onClick={() => setFlag(false)}><Text>All Submissions</Text></Button>
-        <Button colorScheme="blue" paddingInline={"20px"} m={"10px"} onClick={() => setFlag(true)}><Text>Favourite Submissions</Text></Button>
-      </SimpleGrid>
-      {!flag ? (
-        submissions.length === 0 ? <Heading marginTop={"10px"}  >No submissions Yet</Heading> : <Box className="container">{subEl}</Box>
-      ) : (
-        <Heading marginTop={"10px"} > Favourites</Heading>
-      )}
+      <Box maxW={"85%"} margin={"auto"} marginTop={"10px"}>
+        <SimpleGrid
+          columns={[1, 2, 2]}
+          maxW={{ sm: "auto", md: "50%", lg: "40%" }}
+        >
+          <Button
+            marginBottom={"10px"}
+            paddingInline={"20px"}
+            onClick={() => setFlag(false)}
+          >
+            <Text>All Submissions</Text>
+          </Button>
+          <Button
+            colorScheme="blue"
+            paddingInline={"20px"}
+            onClick={() => setFlag(true)}
+          >
+            <Text>Favourite Submissions</Text>
+          </Button>
+        </SimpleGrid>
+        {!flag ? (
+          submissions.length === 0 ? (
+            <Heading marginTop={"10px"}>No submissions Yet</Heading>
+          ) : (
+            <Box className="container">{subEl}</Box>
+          )
+        ) : (
+          <Heading marginTop={"10px"}> Favourites</Heading>
+        )}
       </Box>
       {/* <div className="maxWidthDiv">
         <button className="home-btn" onClick={() => setFlag(false)}>
@@ -56,7 +74,6 @@ const Home = () => {
       ) : (
         <h1 className="navbar"> Favourites</h1>
       )} */}
-
     </div>
   );
 };
